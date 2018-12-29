158035586e4429baa10c58e9993178481336146c
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2018-12-29 16:15:44 -0600

    StepperOutputService_StepDirectionControl

--- libs/EmbeddedIOServices/src/IOServices/StepperOutputService/IStepperOutputService.cpp
+++ libs/EmbeddedIOServices/src/IOServices/StepperOutputService/IStepperOutputService.cpp
@@ -1,4 +1,5 @@
 #include "IOServices/StepperOutputService/IStepperOutputService.h"
+#include "IOServices/StepperOutputService/StepperOutputService_StepDirectionControl.h"
 
 #ifdef ISTEPPEROUTPUTSERVICE_H
 namespace IOServices
@@ -13,14 +14,28 @@ namespace IOServices
 		
 		switch (stepperServiceId)
 		{
-#ifdef STEPPERSERVICE_STEPDIRECTIONCONTROL_H
+#ifdef STEPPEROUTPUTSERVICE_STEPDIRECTIONCONTROL_H
 		case 1:
-			outputService = new StepperService_StepDirectionControl(config);
+			StepperOutputService_StepDirectionControlConfig *stepperConfig = StepperOutputService_StepDirectionControlConfig::Cast(config);
+			*sizeOut += stepperConfig->Size();
+
+			config = (void*)((unsigned char *)config + stepperConfig->Size());
+			unsigned int subSize = 0;
+			IBooleanOutputService *stepBooleanOutputService = IBooleanOutputService::CreateBooleanOutputService(hardwareAbstractionCollection, config, &subSize);
+			*sizeOut += subSize;
+
+			config = (void*)((unsigned char *)config + subSize);
+			IBooleanOutputService *directionBooleanOutputService = IBooleanOutputService::CreateBooleanOutputService(hardwareAbstractionCollection, config, &subSize);
+			*sizeOut += subSize;
+
+			outputService = new StepperOutputService_StepDirectionControl(hardwareAbstractionCollection, stepperConfig, stepBooleanOutputService, directionBooleanOutputService);
+			break;
 #endif
 			
-#ifdef STEPPERSERVICE_COILCONTROL_H
+#ifdef STEPPEROUTPUTSERVICE_COILCONTROL_H
 		case 2:
-			outputService = new StepperService_CoilControl(config);
+			outputService = new StepperOutputService_CoilControl(hardwareAbstractionCollection, config);
+			break;
 #endif
 		}
 		
