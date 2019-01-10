23ec10315e9ccd705a59f8a6fd5d78d59eac5d2f
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2019-01-09 23:51:59 -0600

    Button Service

---
+++ libs/EmbeddedIOServices/include/IOServices/ButtonService/IButtonService.h
@@ -0,0 +1,23 @@
+#include "HardwareAbstraction/HardwareAbstractionCollection.h"
+
+using namespace HardwareAbstraction;
+
+#ifndef IBUTTONSERVICE_H
+#define IBUTTONSERVICE_H
+namespace IOServices
+{
+	class IButtonService
+	{
+	protected:
+		CallBackGroup *_callBackGroup;
+		IButtonService();
+	public:
+		void Add(ICallBack *callBack);
+		void Add(void(*callBackPointer)(void *), void *parameters);
+		void Remove(ICallBack *callBack);
+		void Clear();
+
+		static IButtonService* CreateButtonService(const HardwareAbstractionCollection *hardwareAbstractionCollection, void *config, unsigned int *sizeOut);
+	};
+}
+#endif
