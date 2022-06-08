319547fce93ba67a957f51acbebd96ccd5f67a1f
FL0WL0W <FL0WL0W@Cracked-Ice.com>
2022-06-08 02:35:51 -0500

    ADC implementation

--- HardwareAbstractions/W806/src/AnalogService_W806.cpp
+++ HardwareAbstractions/W806/src/AnalogService_W806.cpp
@@ -10,7 +10,7 @@ namespace EmbeddedIOServices
         __HAL_RCC_GPIO_CLK_ENABLE();
 		_hadc.Instance = ADC;
 		_hadc.Init.channel = PinToChannel(pin);
-		_hadc.Init.freq = ADC_FREQ_MAX;
+		_hadc.Init.freq = 1000;
 		
 		HAL_ADC_Init(&_hadc);
 	}
@@ -35,5 +35,47 @@ namespace EmbeddedIOServices
 				return ADC_CHANNEL_3;
 		}
 	}
+}
+
+extern "C" void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
+{
+    if (hadc->Instance == ADC)
+    {
+        __HAL_RCC_ADC_CLK_ENABLE();
+        __HAL_RCC_GPIO_CLK_ENABLE();
+        
+        //ADC_CHANNEL_0 : PA1
+        //ADC_CHANNEL_1 : PA4
+        //ADC_CHANNEL_2 : PA3
+        //ADC_CHANNEL_3 : PA2
+        //ADC_CHANNEL_0_1 : PA1 and PA4
+        //ADC_CHANNEL_2_3 : PA3 and PA2
+        if (hadc->Init.channel == ADC_CHANNEL_0)
+        {
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_1);
+        }
+        else if (hadc->Init.channel == ADC_CHANNEL_1)
+        {
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_4);
+        }
+        else if (hadc->Init.channel == ADC_CHANNEL_2)
+        {
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_3);
+        }
+        else if (hadc->Init.channel == ADC_CHANNEL_3)
+        {
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_2);
+        }
+        else if (hadc->Init.channel == ADC_CHANNEL_0_1)
+        {
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_1);
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_4);
+        }
+        else if (hadc->Init.channel == ADC_CHANNEL_2_3)
+        {
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_3);
+            __HAL_AFIO_REMAP_ADC(GPIOA, GPIO_PIN_2);
+        }
+    }
 }
 #endif
