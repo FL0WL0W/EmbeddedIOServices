d0551cca170d6116a95e8c2906196a52cb9e59a6
FL0WL0W <dhjohnson5@gmail.com>
2024-11-29 20:05:25 -0600

    Websocket aggregate packets

--- HardwareAbstractions/Esp32Idf/include/Esp32IdfCommunicationService_WebSocket.h
+++ HardwareAbstractions/Esp32Idf/include/Esp32IdfCommunicationService_WebSocket.h
@@ -9,11 +9,15 @@ namespace Esp32
 	class Esp32IdfCommunicationService_WebSocket : public EmbeddedIOServices::ICommunicationService
 	{
 	protected:
-		uint8_t buf[1024];
+		uint8_t _txBuf[1024];
+		size_t _txBufStartIndex = 0;
+		size_t _txBufEndIndex = 0;
+		bool _txFlushing = false;
 		httpd_handle_t _server;
 		std::list<int> _fds;
 	public:
         Esp32IdfCommunicationService_WebSocket(httpd_handle_t server, const char *uri);
+		void Flush();
         void Send(const void *data, size_t length);
 	};
 }
--- HardwareAbstractions/Esp32Idf/src/Esp32IdfCommunicationService_WebSocket.cpp
+++ HardwareAbstractions/Esp32Idf/src/Esp32IdfCommunicationService_WebSocket.cpp
@@ -19,14 +19,19 @@ namespace Esp32
 				}
 				httpd_ws_frame_t ws_pkt;
 				memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
-        		ws_pkt.payload = commService->buf;
-				esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 1024);
+				esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
 				if (ret == ESP_OK) 
 				{
-					if(ws_pkt.type == HTTPD_WS_TYPE_BINARY)
-						commService->Receive(ws_pkt.payload, ws_pkt.len);
-					if(ws_pkt.type == HTTPD_WS_TYPE_CLOSE)
-						commService->_fds.remove(httpd_req_to_sockfd(req));
+					ws_pkt.payload = reinterpret_cast<uint8_t*>(malloc(ws_pkt.len));
+					ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+					if (ret == ESP_OK) 
+					{
+						if(ws_pkt.type == HTTPD_WS_TYPE_BINARY)
+							commService->Receive(ws_pkt.payload, ws_pkt.len);
+						if(ws_pkt.type == HTTPD_WS_TYPE_CLOSE)
+							commService->_fds.remove(httpd_req_to_sockfd(req));
+					}
+					free(ws_pkt.payload);
 				}
 
 				return ESP_OK;
@@ -39,21 +44,69 @@ namespace Esp32
         httpd_register_uri_handler(_server, &ws);
 	}
 	
-	void Esp32IdfCommunicationService_WebSocket::Send(const void *data, size_t length)
+	void Esp32IdfCommunicationService_WebSocket::Flush()
 	{
+		if(_txFlushing)
+			return;
+		_txFlushing = true;
 		for(auto next = _fds.begin(); next != _fds.end(); ++next)
 		{
-			httpd_ws_frame_t *ws_pkt = reinterpret_cast<httpd_ws_frame_t *>(calloc(1, sizeof(httpd_ws_frame_t)));
-			ws_pkt->len = length;
-			ws_pkt->type = HTTPD_WS_TYPE_BINARY;
-			ws_pkt->payload = reinterpret_cast<uint8_t*>(malloc(length));
-			memcpy(ws_pkt->payload, data, length);
-    		httpd_ws_send_data_async(_server, (*next), ws_pkt, [](esp_err_t err, int socket, void *arg) 
+			size_t length = _txBufEndIndex;
+			length += (length < _txBufStartIndex? sizeof(_txBuf) : 0) - _txBufStartIndex;
+
+			if(length < 1)
+			{
+				_txFlushing = false;
+				return;
+			}
+
+			struct ws_send_arg_t
+			{
+				Esp32IdfCommunicationService_WebSocket *commService;
+				httpd_ws_frame_t ws_pkt;
+			};
+			ws_send_arg_t *ws_send_arg = reinterpret_cast<ws_send_arg_t *>(calloc(1, sizeof(ws_send_arg_t)));
+			ws_send_arg->commService = this;
+		
+			ws_send_arg->ws_pkt.len = length;
+			ws_send_arg->ws_pkt.type = HTTPD_WS_TYPE_BINARY;
+			ws_send_arg->ws_pkt.payload = reinterpret_cast<uint8_t*>(malloc(ws_send_arg->ws_pkt.len));
+			if(_txBufStartIndex + ws_send_arg->ws_pkt.len >= sizeof(_txBuf))
+			{
+				const size_t lengthToEnd = sizeof(_txBuf) - _txBufStartIndex;
+				memcpy(ws_send_arg->ws_pkt.payload, &_txBuf[_txBufStartIndex], lengthToEnd);
+				memcpy(&ws_send_arg->ws_pkt.payload[lengthToEnd], _txBuf, ws_send_arg->ws_pkt.len - lengthToEnd);
+			}
+			else
+			{
+				memcpy(ws_send_arg->ws_pkt.payload, &_txBuf[_txBufStartIndex], ws_send_arg->ws_pkt.len);
+			}
+			_txBufStartIndex = (_txBufStartIndex + ws_send_arg->ws_pkt.len) % sizeof(_txBuf);
+
+    		httpd_ws_send_data_async(_server, (*next), &ws_send_arg->ws_pkt, [](esp_err_t err, int socket, void *arg) 
 			{ 
-				httpd_ws_frame_t *ws_pkt = reinterpret_cast<httpd_ws_frame_t *>(arg);
-				free(ws_pkt->payload);
-				free(ws_pkt);
-			}, ws_pkt);
+				ws_send_arg_t *ws_send_arg = reinterpret_cast<ws_send_arg_t *>(arg);
+				free(ws_send_arg->ws_pkt.payload);
+				ws_send_arg->commService->_txFlushing = false;
+				ws_send_arg->commService->Flush();
+				free(ws_send_arg);
+			}, ws_send_arg);
 		}
+	}
+
+	void Esp32IdfCommunicationService_WebSocket::Send(const void *data, size_t length)
+	{
+		if(_txBufEndIndex + length >= sizeof(_txBuf))
+		{
+			const size_t lengthToEnd = sizeof(_txBuf) - _txBufEndIndex;
+			memcpy(&_txBuf[_txBufEndIndex], data, lengthToEnd);
+			memcpy(_txBuf, &reinterpret_cast<const uint8_t *>(data)[lengthToEnd], length - lengthToEnd);
+		}
+		else
+		{
+			memcpy(&_txBuf[_txBufEndIndex], data, length);
+		}
+		_txBufEndIndex = (_txBufEndIndex + length) % sizeof(_txBuf);
+		Flush();
 	}
 }
