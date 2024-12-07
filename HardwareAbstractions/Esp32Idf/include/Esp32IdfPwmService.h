ac3bfaaaea65ff10aa9eee1fce130c67987196bb
FL0WL0W <dhjohnson5@gmail.com>
2024-12-07 02:10:22 -0600

    ESP32IDF PWM Service using LEDC

--- HardwareAbstractions/Esp32Idf/include/Esp32IdfPwmService.h
+++ HardwareAbstractions/Esp32Idf/include/Esp32IdfPwmService.h
@@ -1,14 +1,23 @@
 #include "PinDirection.h"
 #include "IPwmService.h"
 #include "stdint.h"
+#include <map>
 
 #ifndef ESP32IDFPWMSERVICE_H
 #define ESP32IDFPWMSERVICE_H
 
 namespace Esp32
 {
+	struct Esp32IdfPwmHandle
+	{
+		uint8_t Timer;
+		uint8_t Channel;
+	};
 	class Esp32IdfPwmService : public EmbeddedIOServices::IPwmService
 	{
+	protected:
+		std::map<EmbeddedIOServices::pwmpin_t, Esp32IdfPwmHandle> _pinHandleMap;
+		uint8_t _ledcTimerIdx = 0;
 	public:
 		void InitPin(EmbeddedIOServices::pwmpin_t pin, EmbeddedIOServices::PinDirection direction, uint16_t minFrequency);
 		EmbeddedIOServices::PwmValue ReadPin(EmbeddedIOServices::pwmpin_t pin);
