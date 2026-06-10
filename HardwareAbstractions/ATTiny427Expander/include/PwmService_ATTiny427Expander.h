5d92a5d05e2c3792b2339e96755eafef40bb7eaf
FL0WL0W <dhjohnson5@gmail.com>
2026-06-09 19:33:12 -0500

    implement all of TCA outputs for ATTiny427 PWM

--- HardwareAbstractions/ATTiny427Expander/include/PwmService_ATTiny427Expander.h
+++ HardwareAbstractions/ATTiny427Expander/include/PwmService_ATTiny427Expander.h
@@ -8,10 +8,15 @@
 #define OFFSET_TCA			0x0A00
 #define ADDRESS_TCA_CTRLA 	(OFFSET_TCA + 0x00)
 #define ADDRESS_TCA_CTRLB 	(OFFSET_TCA + 0x01)
-#define ADDRESS_TCA_PERBUF 	(OFFSET_TCA + 0x36)
-#define ADDRESS_TCA_CMP0BUF	(OFFSET_TCA + 0x38)
-#define ADDRESS_TCA_CMP1BUF	(OFFSET_TCA + 0x3A)
-#define ADDRESS_TCA_CMP2BUF	(OFFSET_TCA + 0x3C)
+#define ADDRESS_TCA_CTRLD 	(OFFSET_TCA + 0x04)
+#define ADDRESS_TCA_PERBUFL 	(OFFSET_TCA + 0x36)
+#define ADDRESS_TCA_PERBUFH 	(OFFSET_TCA + 0x37)
+#define ADDRESS_TCA_CMP0BUFL	(OFFSET_TCA + 0x38)
+#define ADDRESS_TCA_CMP0BUFH	(OFFSET_TCA + 0x39)
+#define ADDRESS_TCA_CMP1BUFL	(OFFSET_TCA + 0x3A)
+#define ADDRESS_TCA_CMP1BUFH	(OFFSET_TCA + 0x3B)
+#define ADDRESS_TCA_CMP2BUFL	(OFFSET_TCA + 0x3C)
+#define ADDRESS_TCA_CMP2BUFH	(OFFSET_TCA + 0x3D)
 
 #define OFFSET_PORTMUX				0x05E0
 #define ADDRESS_PORTMUX_TCAROUTEA	(OFFSET_PORTMUX + 0x04)
@@ -24,12 +29,22 @@ namespace EmbeddedIOServices
 		const ATTiny427_ExpanderComm _comm;
 		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CTRLA;
 		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CTRLB;
-		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_PERBUF;
-		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP0BUF;
-		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP1BUF;
-		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP2BUF;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CTRLD;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_PERBUFL;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_PERBUFH;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP0BUFL;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP0BUFH;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP1BUFL;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP1BUFH;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP2BUFL;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _TCA_CMP2BUFH;
 		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _PORTMUX_TCAROUTEA;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _VPORTA_DIR;
 		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _VPORTB_DIR;
+		ATTiny427_ExpanderService::Attiny427_ExpanderRegister & _VPORTC_DIR;
+
+		void SetupTCADiv8Bit(uint16_t minFrequency);
+		void SetupTCADiv16Bit(uint16_t minFrequency);
 
 	public:
 		PwmService_ATTiny427Expander(ATTiny427_ExpanderService *aTTiny427ExpanderService);
--- HardwareAbstractions/ATTiny427Expander/src/PwmService_ATTiny427Expander.cpp
+++ HardwareAbstractions/ATTiny427Expander/src/PwmService_ATTiny427Expander.cpp
@@ -7,14 +7,204 @@ namespace EmbeddedIOServices
 		_comm(aTTiny427ExpanderService->Comm),
 		_TCA_CTRLA(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CTRLA)),
 		_TCA_CTRLB(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CTRLB)),
-		_TCA_PERBUF(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_PERBUF)),
-		_TCA_CMP0BUF(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP0BUF)),
-		_TCA_CMP1BUF(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP1BUF)),
-		_TCA_CMP2BUF(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP2BUF)),
+		_TCA_CTRLD(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CTRLD)),
+		_TCA_PERBUFL(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_PERBUFL)),
+		_TCA_PERBUFH(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_PERBUFH)),
+		_TCA_CMP0BUFL(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP0BUFL)),
+		_TCA_CMP0BUFH(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP0BUFH)),
+		_TCA_CMP1BUFL(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP1BUFL)),
+		_TCA_CMP1BUFH(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP1BUFH)),
+		_TCA_CMP2BUFL(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP2BUFL)),
+		_TCA_CMP2BUFH(aTTiny427ExpanderService->GetRegister(ADDRESS_TCA_CMP2BUFH)),
 		_PORTMUX_TCAROUTEA(aTTiny427ExpanderService->GetRegister(ADDRESS_PORTMUX_TCAROUTEA)),
-		_VPORTB_DIR(aTTiny427ExpanderService->GetRegister(ADDRESS_VPORTB_DIR))
+		_VPORTA_DIR(aTTiny427ExpanderService->GetRegister(ADDRESS_VPORTA_DIR)),
+		_VPORTB_DIR(aTTiny427ExpanderService->GetRegister(ADDRESS_VPORTB_DIR)),
+		_VPORTC_DIR(aTTiny427ExpanderService->GetRegister(ADDRESS_VPORTC_DIR))
     {
     }
+
+	void PwmService_ATTiny427Expander::SetupTCADiv16Bit(uint16_t minFrequency)
+	{
+		const uint16_t divExact = (20000000 / (minFrequency * ((1 << 16) - 1)));
+		uint8_t clksel = 0;
+		uint16_t div = 1;
+		if(divExact > 255)
+		{
+			clksel = 0x7;
+			div = 1024;
+		}
+		else if(divExact > 63)
+		{
+			clksel = 0x6;
+			div = 256;
+		}
+		else if(divExact > 15)
+		{
+			clksel = 0x5;
+			div = 64;
+		}
+		else if(divExact > 7)
+		{
+			clksel = 0x4;
+			div = 16;
+		}
+		else if(divExact > 3)
+		{
+			clksel = 0x3;
+			div = 8;
+		}
+		else if(divExact > 1)
+		{
+			clksel = 0x2;
+			div = 4;
+		}
+		else if(divExact > 0)
+		{
+			clksel = 0x1;
+			div = 2;
+		}
+		uint16_t divCurrent = 1;
+		switch(_TCA_CTRLA & 0xE)
+		{
+			case (1 << 1):
+				divCurrent = 2;
+				break;
+			case (2 << 1):
+				divCurrent = 4;
+				break;
+			case (3 << 1):
+				divCurrent = 8;
+				break;
+			case (4 << 1):
+				divCurrent = 16;
+				break;
+			case (5 << 1):
+				divCurrent = 64;
+				break;
+			case (6 << 1):
+				divCurrent = 256;
+				break;
+			case (7 << 1):
+				divCurrent = 1024;
+				break;
+		}
+		const uint16_t per = (static_cast<uint16_t>(_TCA_PERBUFL) | (static_cast<uint16_t>(_TCA_PERBUFH) << 8)) * divCurrent / div;
+		const uint16_t cmp0 = (static_cast<uint16_t>(_TCA_CMP0BUFL) | (static_cast<uint16_t>(_TCA_CMP0BUFH) << 8)) * divCurrent / div;
+		const uint16_t cmp1 = (static_cast<uint16_t>(_TCA_CMP1BUFL) | (static_cast<uint16_t>(_TCA_CMP1BUFH) << 8)) * divCurrent / div;
+		const uint16_t cmp2 = (static_cast<uint16_t>(_TCA_CMP2BUFL) | (static_cast<uint16_t>(_TCA_CMP2BUFH) << 8)) * divCurrent / div;
+		_TCA_CTRLA = (_TCA_CTRLA & 0xF0) | (clksel << 1);
+		if(_TCA_CTRLD == 0x1)
+		{
+			_TCA_CTRLD = 0x0;
+			_TCA_CTRLB = ((_TCA_CTRLB & 0x0F) << 4) | 0x3;
+		}
+		_TCA_PERBUFH = (per >> 8) & 0xFF;
+		_TCA_PERBUFL = per & 0xFF;
+		_TCA_CMP0BUFH = (cmp0 >> 8) & 0xFF;
+		_TCA_CMP0BUFL = cmp0 & 0xFF;
+		_TCA_CMP1BUFH = (cmp1 >> 8) & 0xFF;
+		_TCA_CMP1BUFL = cmp1 & 0xFF;
+		_TCA_CMP2BUFH = (cmp2 >> 8) & 0xFF;
+		_TCA_CMP2BUFL = cmp2 & 0xFF;
+		_TCA_CTRLA |= 0x1;
+	}
+
+	void PwmService_ATTiny427Expander::SetupTCADiv8Bit(uint16_t minFrequency)
+	{
+		const uint16_t divExact = (20000000 / (minFrequency * ((1 << 8) - 1)));
+		uint8_t clksel = 0;
+		uint16_t div = 1;
+		if(divExact > 255)
+		{
+			clksel = 0x7;
+			div = 1024;
+		}
+		else if(divExact > 63)
+		{
+			clksel = 0x6;
+			div = 256;
+		}
+		else if(divExact > 15)
+		{
+			clksel = 0x5;
+			div = 64;
+		}
+		else if(divExact > 7)
+		{
+			clksel = 0x4;
+			div = 16;
+		}
+		else if(divExact > 3)
+		{
+			clksel = 0x3;
+			div = 8;
+		}
+		else if(divExact > 1)
+		{
+			clksel = 0x2;
+			div = 4;
+		}
+		else if(divExact > 0)
+		{
+			clksel = 0x1;
+			div = 2;
+		}
+		uint16_t divCurrent = 1;
+		switch(_TCA_CTRLA & 0xE)
+		{
+			case (1 << 1):
+				divCurrent = 2;
+				break;
+			case (2 << 1):
+				divCurrent = 4;
+				break;
+			case (3 << 1):
+				divCurrent = 8;
+				break;
+			case (4 << 1):
+				divCurrent = 16;
+				break;
+			case (5 << 1):
+				divCurrent = 64;
+				break;
+			case (6 << 1):
+				divCurrent = 256;
+				break;
+			case (7 << 1):
+				divCurrent = 1024;
+				break;
+		}
+		_TCA_CTRLA = (_TCA_CTRLA & 0xF0) | (clksel << 1);
+		if(_TCA_CTRLD == 0x1)
+		{
+			_TCA_PERBUFH = (static_cast<uint8_t>(_TCA_PERBUFH) * divCurrent / div);
+			_TCA_PERBUFL = (static_cast<uint8_t>(_TCA_PERBUFL) * divCurrent / div);
+			_TCA_CMP0BUFH = (static_cast<uint8_t>(_TCA_CMP0BUFH) * divCurrent / div);
+			_TCA_CMP0BUFL = (static_cast<uint8_t>(_TCA_CMP0BUFL) * divCurrent / div);
+			_TCA_CMP1BUFH = (static_cast<uint8_t>(_TCA_CMP1BUFH) * divCurrent / div);
+			_TCA_CMP1BUFL = (static_cast<uint8_t>(_TCA_CMP1BUFL) * divCurrent / div);
+			_TCA_CMP2BUFH = (static_cast<uint8_t>(_TCA_CMP2BUFH) * divCurrent / div);
+			_TCA_CMP2BUFL = (static_cast<uint8_t>(_TCA_CMP2BUFL) * divCurrent / div);
+		}
+		else
+		{
+			const uint16_t per = (static_cast<uint16_t>(_TCA_PERBUFL) | (static_cast<uint16_t>(_TCA_PERBUFH) << 8)) * divCurrent / div;
+			const uint16_t cmp0 = (static_cast<uint16_t>(_TCA_CMP0BUFL) | (static_cast<uint16_t>(_TCA_CMP0BUFH) << 8)) * divCurrent / div;
+			const uint16_t cmp1 = (static_cast<uint16_t>(_TCA_CMP1BUFL) | (static_cast<uint16_t>(_TCA_CMP1BUFH) << 8)) * divCurrent / div;
+			const uint16_t cmp2 = (static_cast<uint16_t>(_TCA_CMP2BUFL) | (static_cast<uint16_t>(_TCA_CMP2BUFH) << 8)) * divCurrent / div;
+			_TCA_CTRLB = (_TCA_CTRLB & 0xF0) >> 4;
+			_TCA_CTRLD = 0x1;
+			_TCA_PERBUFH = 0xFF;
+			_TCA_PERBUFL = per;
+			_TCA_CMP0BUFH = 0xFF;
+			_TCA_CMP0BUFL = cmp0 & 0xFF;
+			_TCA_CMP1BUFH = 0xFF;
+			_TCA_CMP1BUFL = cmp1 & 0xFF;
+			_TCA_CMP2BUFH = 0xFF;
+			_TCA_CMP2BUFL = cmp2 & 0xFF;
+		}
+		_TCA_CTRLA |= 0x1;
+	}
 
 	void PwmService_ATTiny427Expander::InitPin(pwmpin_t pin, PinDirection direction, uint16_t minFrequency)
 	{
@@ -41,124 +231,100 @@ namespace EmbeddedIOServices
 
 		if(direction == Out)
 		{
-			const uint16_t divExact = (20000000 / (minFrequency * ((1 << 16) - 1)));
-			uint8_t clksel = 0;
-			uint16_t div = 1;
-			if(divExact > 255)
-			{
-				clksel = 0x7;
-				div = 1024;
-			}
-			else if(divExact > 63)
-			{
-				clksel = 0x6;
-				div = 256;
-			}
-			else if(divExact > 15)
-			{
-				clksel = 0x5;
-				div = 64;
-			}
-			else if(divExact > 7)
-			{
-				clksel = 0x4;
-				div = 16;
-			}
-			else if(divExact > 3)
-			{
-				clksel = 0x3;
-				div = 8;
-			}
-			else if(divExact > 1)
-			{
-				clksel = 0x2;
-				div = 4;
-			}
-			else if(divExact > 0)
-			{
-				clksel = 0x1;
-				div = 2;
-			}
-			uint16_t divCurrent = 1;
-			switch(_TCA_CTRLA & 0xE)
-			{
-				case (1 << 1):
-					divCurrent = 2;
-					break;
-				case (2 << 1):
-					divCurrent = 4;
-					break;
-				case (3 << 1):
-					divCurrent = 8;
-					break;
-				case (4 << 1):
-					divCurrent = 16;
-					break;
-				case (5 << 1):
-					divCurrent = 64;
-					break;
-				case (6 << 1):
-					divCurrent = 256;
-					break;
-				case (7 << 1):
-					divCurrent = 1024;
-					break;
-			}
-			_TCA_CTRLA = (_TCA_CTRLA & 0xF1) | (clksel << 1);
-			_TCA_PERBUF = static_cast<uint32_t>(_TCA_PERBUF) * divCurrent / div;
-			_TCA_CMP0BUF = static_cast<uint32_t>(_TCA_CMP0BUF) * divCurrent / div;
-			_TCA_CMP1BUF = static_cast<uint32_t>(_TCA_CMP1BUF) * divCurrent / div;
-			_TCA_CMP2BUF = static_cast<uint32_t>(_TCA_CMP2BUF) * divCurrent / div;
 			switch(pin)
 			{
-				// TODO: All the ones that are commented out. the below is all i need right now for expander
-				// case 3: //TCAWO3 or TCB1WO
-				// 	break;
-				// case 4: //TCAWO4
-				// 	break;
-				// case 5: //TCAWO5 or TCB0WO
-				// 	break;
+				case 3: //TCAWO3 or TODO TCB1WO
+					SetupTCADiv8Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | 0x10; 
+					_PORTMUX_TCAROUTEA &= ~0x8; //set TCA WO4 to PC3
+					_VPORTC_DIR |= 0x08;
+					break;
+				case 4: //TCAWO4
+					SetupTCADiv8Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | 0x20; 
+					_PORTMUX_TCAROUTEA &= ~0x10; //set TCA WO4 to PA4
+					_VPORTA_DIR |= 0x10;
+					break;
+				case 5: //TCAWO5 or TODO TCB0WO
+					SetupTCADiv8Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | 0x40; 
+					_PORTMUX_TCAROUTEA &= ~0x20; //set TCA WO5 to PC5
+					_VPORTC_DIR |= 0x20;
+					break;
 				case 8: //TCAWO0
-					_TCA_CTRLA |= 0x1; 
-					_TCA_CTRLB = (_TCA_CTRLB & 0xF0) | 0x13; 
+					if(_TCA_CTRLD == 0x1 && (_TCA_CTRLB & 0x7) != 0)
+						SetupTCADiv8Bit(minFrequency);
+					else
+						SetupTCADiv16Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | (_TCA_CTRLD == 0x1? 0x01 : 0x10); 
+					_PORTMUX_TCAROUTEA &= ~0x1; //set TCA WO0 to PB0
 					_VPORTB_DIR |= 0x01;
 					break;
 				case 9: //TCAWO1
-					_TCA_CTRLA |= 0x1; 
-					_TCA_CTRLB = (_TCA_CTRLB & 0xF0) | 0x23; 
+					if(_TCA_CTRLD == 0x1 && (_TCA_CTRLB & 0x7) != 0)
+						SetupTCADiv8Bit(minFrequency);
+					else
+						SetupTCADiv16Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | (_TCA_CTRLD == 0x1? 0x02 : 0x20); 
+					_PORTMUX_TCAROUTEA &= ~0x2; //set TCA WO1 to PB1
 					_VPORTB_DIR |= 0x02;
 					break;
 				case 10: //TCAWO2
-					_TCA_CTRLA |= 0x1;
-					_TCA_CTRLB = (_TCA_CTRLB & 0xF0) | 0x43; 
+					if(_TCA_CTRLD == 0x1 && (_TCA_CTRLB & 0x7) != 0)
+						SetupTCADiv8Bit(minFrequency);
+					else
+						SetupTCADiv16Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | (_TCA_CTRLD == 0x1? 0x04 : 0x40); 
+					_PORTMUX_TCAROUTEA &= ~0x4; //set TCA WO2 to PB2
 					_VPORTB_DIR |= 0x04;
 					break;
-				case 11: //TCAWO0
-					_TCA_CTRLA |= 0x1; 
-					_TCA_CTRLB = (_TCA_CTRLB & 0xF0) | 0x13; 
+				case 11: //TCAWO0'
+					if(_TCA_CTRLD == 0x1 && (_TCA_CTRLB & 0x7) != 0)
+						SetupTCADiv8Bit(minFrequency);
+					else
+						SetupTCADiv16Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | (_TCA_CTRLD == 0x1? 0x01 : 0x10);; 
 					_PORTMUX_TCAROUTEA |= 0x1; //set TCA WO0 to PB3
 					_VPORTB_DIR |= 0x08;
 					break;
-				case 12: //TCAWO1
-					_TCA_CTRLA |= 0x1; 
-					_TCA_CTRLB = (_TCA_CTRLB & 0xF0) | 0x23; 
+				case 12: //TCAWO1'
+					if(_TCA_CTRLD == 0x1 && (_TCA_CTRLB & 0x7) != 0)
+						SetupTCADiv8Bit(minFrequency);
+					else
+						SetupTCADiv16Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | (_TCA_CTRLD == 0x1? 0x02 : 0x20); 
 					_PORTMUX_TCAROUTEA |= 0x2; //set TCA WO1 to PB4
 					_VPORTB_DIR |= 0x10;
 					break;
-				case 13: //TCAWO2
-					_TCA_CTRLA |= 0x1;
-					_TCA_CTRLB = (_TCA_CTRLB & 0xF0) | 0x43; 
+				case 13: //TCAWO2'
+					if(_TCA_CTRLD == 0x1 && (_TCA_CTRLB & 0x7) != 0)
+						SetupTCADiv8Bit(minFrequency);
+					else
+						SetupTCADiv16Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | (_TCA_CTRLD == 0x1? 0x04 : 0x40); 
 					_PORTMUX_TCAROUTEA |= 0x4; //set TCA WO2 to PB5
 					_VPORTB_DIR |= 0x20;
 					break;
 				// case 16: //TCB0WO'
 				// 	break;
-				// case 19: //TCAWO3`
-				// 	break;
-				// case 20: //TCAWO4' or TCB1WO'
-				// 	break;
-				// case 21: //TCAWO5'
-				// 	break;
+				case 19: //TCAWO3`
+					SetupTCADiv8Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | 0x10; 
+					_PORTMUX_TCAROUTEA |= 0x8; //set TCA WO4 to PC3
+					_VPORTC_DIR |= 0x08;
+					break;
+				case 20: //TCAWO4' or TODO TCB1WO'
+					SetupTCADiv8Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | 0x20; 
+					_PORTMUX_TCAROUTEA |= 0x10; //set TCA WO4 to PC4
+					_VPORTC_DIR |= 0x10;
+					break;
+				case 21: //TCAWO5'
+					SetupTCADiv8Bit(minFrequency);
+					_TCA_CTRLB = _TCA_CTRLB | 0x40; 
+					_PORTMUX_TCAROUTEA |= 0x20; //set TCA WO5 to PC5
+					_VPORTC_DIR |= 0x20;
+					break;
 			}
 		}
 		else
@@ -211,44 +377,83 @@ namespace EmbeddedIOServices
 		switch(pin)
 		{
 			// TODO: All the ones that are commented out. the below is all i need right now for expander
-			// case 3: //TCAWO3 or TCB1WO
-			// 	break;
-			// case 4: //TCAWO4
-			// 	break;
-			// case 5: //TCAWO5 or TCB0WO
-			// 	break;
+			case 3: //TCAWO3 or TODO TCB1WO
+				_TCA_PERBUFH = value.Period * 20000000 / divCurrent + 0.5f;
+				_TCA_CMP0BUFH = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				break;
+			case 4: //TCAWO4
+				_TCA_PERBUFH = value.Period * 20000000 / divCurrent + 0.5f;
+				_TCA_CMP1BUFH = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				break;
+			case 5: //TCAWO5 or TODO TCB0WO
+				_TCA_PERBUFH = value.Period * 20000000 / divCurrent + 0.5f;
+				_TCA_CMP2BUFH = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				break;
 			case 8: //TCAWO0
-				_TCA_PERBUF = value.Period * 20000000 / divCurrent;
-				_TCA_CMP0BUF = value.PulseWidth * 20000000 / divCurrent;
+			case 11: //TCAWO0'
+				const uint16_t per = value.Period * 20000000 / divCurrent + 0.5f;
+				const uint16_t cmp0 = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				if(_TCA_CTRLD == 0x1)
+				{
+					_TCA_PERBUFH = (per >> 8) & 0xFF;
+					_TCA_PERBUFL = per & 0xFF;
+					_TCA_CMP0BUFH = (cmp0 >> 8) & 0xFF;
+					_TCA_CMP0BUFL = cmp0 & 0xFF;
+				}
+				else
+				{
+					_TCA_PERBUFL = per;
+					_TCA_CMP0BUFL = cmp0;
+				}
 				break;
 			case 9: //TCAWO1
-				_TCA_PERBUF = value.Period * 20000000 / divCurrent;
-				_TCA_CMP1BUF = value.PulseWidth * 20000000 / divCurrent;
+			case 12: //TCAWO1'
+				const uint16_t per = value.Period * 20000000 / divCurrent + 0.5f;
+				const uint16_t cmp1 = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				if(_TCA_CTRLD == 0x1)
+				{
+					_TCA_PERBUFH = (per >> 8) & 0xFF;
+					_TCA_PERBUFL = per & 0xFF;
+					_TCA_CMP1BUFH = (cmp1 >> 8) & 0xFF;
+					_TCA_CMP1BUFL = cmp1 & 0xFF;
+				}
+				else
+				{
+					_TCA_PERBUFL = per;
+					_TCA_CMP1BUFL = cmp1;
+				}
 				break;
 			case 10: //TCAWO2
-				_TCA_PERBUF = value.Period * 20000000 / divCurrent;
-				_TCA_CMP2BUF = value.PulseWidth * 20000000 / divCurrent;
-				break;
-			case 11: //TCAWO0'
-				_TCA_PERBUF = value.Period * 20000000 / divCurrent;
-				_TCA_CMP0BUF = value.PulseWidth * 20000000 / divCurrent;
-				break;
-			case 12: //TCAWO1'
-				_TCA_PERBUF = value.Period * 20000000 / divCurrent;
-				_TCA_CMP1BUF = value.PulseWidth * 20000000 / divCurrent;
-				break;
 			case 13: //TCAWO2'
-				_TCA_PERBUF = value.Period * 20000000 / divCurrent;
-				_TCA_CMP2BUF = value.PulseWidth * 20000000 / divCurrent;
+				const uint16_t per = value.Period * 20000000 / divCurrent + 0.5f;
+				const uint16_t cmp2 = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				if(_TCA_CTRLD == 0x1)
+				{
+					_TCA_PERBUFH = (per >> 8) & 0xFF;
+					_TCA_PERBUFL = per & 0xFF;
+					_TCA_CMP2BUFH = (cmp2 >> 8) & 0xFF;
+					_TCA_CMP2BUFL = cmp2 & 0xFF;
+				}
+				else
+				{
+					_TCA_PERBUFL = per;
+					_TCA_CMP2BUFL = cmp2;
+				}
 				break;
 			// case 16: //TCB0WO'
 			// 	break;
-			// case 19: //TCAWO3`
-			// 	break;
-			// case 20: //TCAWO4' or TCB1WO'
-			// 	break;
-			// case 21: //TCAWO5'
-			// 	break;
+			case 19: //TCAWO3`
+				_TCA_PERBUFH = value.Period * 20000000 / divCurrent + 0.5f;
+				_TCA_CMP0BUFH = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				break;
+			case 20: //TCAWO4' or TODO TCB1WO'
+				_TCA_PERBUFH = value.Period * 20000000 / divCurrent + 0.5f;
+				_TCA_CMP1BUFH = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				break;
+			case 21: //TCAWO5'
+				_TCA_PERBUFH = value.Period * 20000000 / divCurrent + 0.5f;
+				_TCA_CMP2BUFH = value.PulseWidth * 20000000 / divCurrent + 0.5f;
+				break;
 		}
 	}
 }
