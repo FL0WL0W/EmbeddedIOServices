823441decb698f2621ea9e3e3ee4e6de0a3cf0a9
FL0WL0W <dhjohnson5@gmail.com>
2026-08-01 01:49:18 -0500

    starting MPC5674F hardware abstraction

---
+++ HardwareAbstractions/MPC5674F/include/MPC5674FClockService.h
@@ -0,0 +1,18 @@
+#include "stdint.h"
+
+#ifndef MPC5674FCLOCKSERVICE_H
+#define MPC5674FCLOCKSERVICE_H
+
+#ifndef MPC5674F_CRYSTAL_HZ
+#define MPC5674F_CRYSTAL_HZ 40000000U
+#endif
+
+namespace MPC5674F
+{
+	class MPC5674FClockService
+	{
+	public:
+	};
+}
+
+#endif
---
+++ HardwareAbstractions/MPC5674F/src/MPC5674FClockService.cpp
@@ -0,0 +1,6 @@
+#include "MPC5674FClockService.h"
+#include "MPC5674F.h"
+
+namespace MPC5674F
+{
+}
